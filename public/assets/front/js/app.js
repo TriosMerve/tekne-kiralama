c7335fe6755a1c1b97133121586954101e762ec4
mervekabakci <105459676+mervekabakci@users.noreply.github.com>
2024-12-23 15:46:45 +0300

    tekne-sahibi

--- public/assets/front/js/app.js
+++ public/assets/front/js/app.js
@@ -237,6 +237,7 @@ if (typeof $.fn.niceSelect !== "undefined") {
     $("#" + selectedValue).addClass("show active");
   });
 }
+
 const dropdownToggle = document.querySelector(".languageWrapper .dropdown-toggle");
 
 if (dropdownToggle) { // Eğer element varsa
@@ -312,81 +313,6 @@ $(document).on("click", ".footerTab.customTab .nav-link", function () {
 //   });
 // });
 
-$(document).ready(function () {
-  // Switch durumunu dinle
-  $(".custom-switch-input").on("change", function () {
-    // Şu anki switch'in kapsayıcı .accSwitchHead'ini al
-    const accSwitchHead = $(this).closest(".accSwitchHead");
-
-    // Bu .accSwitchHead'in next'ini (yani ilgili .accSwitchBody) al
-    const accSwitchBody = accSwitchHead.next(".accSwitchBody");
-
-    if ($(this).is(":checked")) {
-      // Eğer switch checked ise: Yüksekliği hesapla ve aç
-      const targetHeight = accSwitchBody.prop("scrollHeight"); // Dinamik yükseklik
-      accSwitchBody.removeClass("disabled");
-    } else {
-      // Eğer switch unchecked ise: Kapat
-      accSwitchBody.addClass("disabled");
-    }
-  });
-
-  $(".mobileSideMenu").on("click", function () {
-    $(this).hasClass("active")
-      ? ($(this).removeClass("active"),
-        $(this).next(".sidenavWrapper").removeClass("active"),
-        $(".overlay").hide(),
-        $("html").removeClass("overflow-hidden"))
-      : ($(this).addClass("active"),
-        $(this).next(".sidenavWrapper").addClass("active"),
-        $(".overlay").show(),
-        $("html").addClass("overflow-hidden"));
-  });
-
-  $(".overlay").on("click", function () {
-    if ($(".mobileSideMenu").hasClass("active")) {
-      $(".mobileSideMenu").removeClass("active");
-      $(".mobileSideMenu").next(".sidenavWrapper").removeClass("active");
-      $(this).hide();
-      $("html").removeClass("overflow-hidden");
-    }
-  });
-
-  //Tekne sahibi hesabım sunulan hizmetler fiyat buton
-  // Radio button durum kontrolü
-  $(".priceRadioWrapper").each(function () {
-    const wrapper = $(this); // Her priceRadioWrapper için işleme başla
-    const radios = wrapper.find('input[type="radio"]'); // Bu wrapper içindeki tüm radio butonlarını al
-    const priceInputWrapper = wrapper.find(".priceInputWrapper"); // Fiyat input alanını seç
-
-    // Sayfa yüklendiğinde radio butonlarının başlangıç durumunu kontrol et
-    const initialRadio = radios.filter(":checked"); // Seçili olan radio butonunu al
-    console.log(
-      "Başlangıç durumunda seçili radio:",
-      initialRadio.length ? initialRadio.val() : "Seçili yok"
-    ); // Başlangıç durumunu logla
-    if (initialRadio.length && initialRadio.hasClass("priceRadioCheck")) {
-      console.log("Ücretli radio butonu seçili, disabled kaldırılıyor");
-      priceInputWrapper.removeClass("disabled"); // Eğer "Ücretli" seçiliyse
-    } else {
-      console.log("Fiyata Dahil radio butonu seçili, disabled ekleniyor");
-      priceInputWrapper.addClass("disabled"); // Eğer "Fiyata Dahil" seçiliyse
-    }
-
-    // Radio button değişikliklerini dinle
-    radios.on("change", function () {
-      const radio = $(this); // Tıklanan radio butonunu al
-      console.log("Tıklanan radio:", radio.val()); // Tıklanan radio butonunu logla
-      if (radio.hasClass("priceRadioCheck") && radio.prop("checked")) {
-        console.log("Ücretli radio butonu seçildi, disabled kaldırılıyor");
-        priceInputWrapper.removeClass("disabled"); // "Ücretli" seçildiğinde fiyat input alanını aktif et
-      } else {
-        console.log("Fiyata Dahil radio butonu seçildi, disabled ekleniyor");
-        priceInputWrapper.addClass("disabled"); // Diğer seçeneklerde fiyat input alanını devre dışı bırak
-      }
-    });
-  });
-});
 
 $(".moreLink").on("click", function(){
   $(this).prev(".line-clamp").toggleClass("line-clamp-3");
